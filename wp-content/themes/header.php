17740bc032b74faf5c39d6b03b902f4bc2492f4a
Michael Beckwith <tw2113@gmail.com>
2016-05-12 23:28:44 -0500

    change header.php to better match buddypress.org

--- wp-content/themes/header.php
+++ wp-content/themes/header.php
@@ -3,11 +3,11 @@
 <head profile="http://gmpg.org/xfn/11">
 <meta charset="utf-8" />
 <!--
-<meta property="fb:page_id" content="6427302910" />
+<meta property="fb:page_id" content="" />
 -->
-<meta name="google-site-verification" content="7VWES_-rcHBcmaQis9mSYamPfNwE03f4vyTj4pfuAw0" />
+<meta name="google-site-verification" content="" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>WordPress</title>
+<title>BuddyPress</title>
 <meta name="referrer" content="always">
 <link href="//s.w.org/wp-includes/css/dashicons.css?20150710" rel="stylesheet" type="text/css" />
 <link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,400,300,600&subset=latin,cyrillic-ext,greek-ext,greek,vietnamese,latin-ext,cyrillic' rel='stylesheet' type='text/css'>
@@ -17,8 +17,8 @@
 
 <script type="text/javascript">
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-52447-1']);
-_gaq.push(['_setDomainName', 'wordpress.org']);
+_gaq.push(['_setAccount', '']);
+_gaq.push(['_setDomainName', 'buddypress.org']);
 _gaq.push(['_trackPageview']);
 (function() {
 	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
@@ -44,44 +44,37 @@ var toggleMenu = function(){
 	    m.className = c.match( ' active' ) ? c.replace( ' active', '' ) : c + ' active';
 }
 </script>
-
-<style type="text/css">
-	@media screen and (max-width:768px) { .survey-wrapper { margin-bottom:20px; } #wporg-header { height: 160px; } #mobile-menu-button { top: 95px; } }
-	.survey-wrapper { width: 100%; height: 0; }
-	.survey-div { font-family: 'Open Sans', Helvetica, Arial, 'Liberation Sans', sans-serif;  font-weight: 600;display: block; margin: 0 auto;  font-size: 13px; color: #fff;height: 22px;width: 335px;text-align:center !important;background-color: #d54e21;border-radius: 0 0 4px 4px; }
-	.survey-div a { color: #fff; font-weight: 500; text-shadow: 1px 1px 3px #666; }
-	@media screen and (max-width:360px) { .survey-div { width: 100%; } }
-</style>
 <?php wp_head(); ?>
+
+<link rel='stylesheet' id='bp-legacy-css-css'  href='https://buddypress.org/wp-content/plugins/buddypress/bp-templates/bp-legacy/css/buddypress.min.css?ver=2.6.0-alpha' type='text/css' media='screen' />
+<link rel='stylesheet' id='bbp-default-css'  href='https://buddypress.org/wp-content/plugins/bbpress/templates/default/css/bbpress.min.css?ver=2.6-alpha-5566' type='text/css' media='screen' />
+<link rel='stylesheet' id='bb-base-css'  href='https://buddypress.org/wp-content/themes/bb-base/style.css?ver=201601200001' type='text/css' media='screen' />
+<link rel='stylesheet' id='bb-root-css'  href='https://buddypress.org/wp-content/themes/bb-base/style-buddypress.css?ver=201601200001' type='text/css' media='screen' />
+<link rel='stylesheet' id='bb-child-css'  href='https://buddypress.org/wp-content/themes/buddypress-org/style.css?ver=201601200001' type='text/css' media='screen' />
+
 </head>
 
-<body id="wordpress-org" <?php body_class(); ?>>
-<div id="wporg-header">
-	<div class="wrapper">
-		<div class="survey-wrapper"><div class="survey-div">
-		<a href="http://wp-survey.polldaddy.com/s/wp-2015">Have you taken the WordPress 2015 Survey yet?</a>
-	</div></div>
-		<a id="mobile-menu-button" class="" href="#" onclick="toggleMenu();"></a>
-	<h1><a href="//wordpress.org">WordPress.org</a></h1>				<div id="head-search">
-		<form action="//wordpress.org/search/do-search.php" method="get">
-			<label for="global-search" class="screen-reader-text">Search WordPress.org for:</label>
-			<input id="global-search" class="text" name="search" type="text" value="" maxlength="150" placeholder="Search WordPress.org" /> <input type="submit" class="button" value="" />
-		</form>
-		</div>
-				<div style="clear:both"></div>
+<body id="top" <?php body_class(); ?>>
+	<dl id="accessibility">
+		<dt>Skip to:</dt>
+		<dd><a href="#content" title="Skip to content">Content</a></dd>
+		<dd><a href="#pages" title="Skip to pages">Pages</a></dd>
+		<dd><a href="#categories" title="Skip to categories">Categories</a></dd>
+		<dd><a href="#search" title="Skip to search">Search</a></dd>
+		<dd class="separator"><a href="#top" title="Skip to top">Top</a></dd>
+		<dd><a href="#bottom" title="Skip to bottom">Bottom</a></dd>
+	</dl>
+	<hr class="hidden" />
 
-<ul id="wporg-header-menu">
-<li><a href='//wordpress.org/showcase/' title='See some of the sites built on WordPress.'>Showcase</a></li>
-<li><a href='//wordpress.org/themes/' title='Find just the right look for your website.'>Themes</a></li>
-<li><a href='//wordpress.org/plugins/' title='Plugins can extend WordPress to do almost anything you can imagine.'>Plugins</a></li>
-<li><a href='//wordpress.org/mobile/' title='Take your website on the go!'>Mobile</a></li>
-<li><a href='//wordpress.org/support/' title='Forums, documentation, help.'>Support</a><ul class="nav-submenu"><li><a href='//wordpress.org/support/' title='Support and discussion forums.'>Forums</a></li><li><a href='//codex.wordpress.org/Main_Page' title='Documentation, tutorials, best practices.'>Documentation</a></li></ul><div class="uparrow"></div></li>
-<li><a href='//make.wordpress.org/' title='Contribute your knowledge.'>Get Involved</a></li>
-<li><a href='//wordpress.org/about/' title='About the WordPress Organization, and where we&#039;re going.'>About</a></li>
-<li><a href='//wordpress.org/news/' title='Come here for the latest scoop.'>Blog</a></li>
-<li><a href='//wordpress.org/hosting/' title='Find a home for your blog.'>Hosting</a></li>
-<li id="download" class="button download-button"><a href='//wordpress.org/download/' title='Get it. Got it? Good.'>Download WordPress</a></li>
-</ul>
-		<div style="clear:both"></div>
-	</div>
-</div>
+	<div id="header"><div id="header-inner">
+		<ul id="nav" class="menu">
+			<li id="menu-item-251086" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-251086"><a href="https://buddypress.org/about/">About</a></li>
+			<li id="menu-item-251087" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-251087"><a href="https://codex.buddypress.org/">Documentation</a></li>
+			<li id="menu-item-251088" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-251088"><a href="https://buddypress.org/blog/">News</a></li>
+			<li id="menu-item-252086" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-252086"><a href="https://codex.buddypress.org/participate-and-contribute/">Get Involved</a></li>
+			<li id="menu-item-251089" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-251089"><a href="https://buddypress.org/support/">Support</a></li>
+			<li id="menu-item-251090" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-251090"><a href="https://buddypress.org/download/">Download</a></li>
+		</ul>
+		<h1><a href="https://buddypress.org">BuddyPress.org</a></h1>
+	</div></div>
+	<hr class="hidden" />
