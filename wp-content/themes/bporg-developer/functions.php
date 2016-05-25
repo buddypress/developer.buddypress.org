b9553a3ecaf81c177609cff5ad59cf98b73d3254
Michael Beckwith <tw2113@gmail.com>
2016-05-25 14:29:36 -0500

    enqueue our css instead of via header.php file

--- wp-content/themes/bporg-developer/functions.php
+++ wp-content/themes/bporg-developer/functions.php
@@ -7,5 +7,10 @@ function bporg_developer_enqueue_css() {
 	wp_enqueue_style( $parent, get_template_directory_uri() . '/style.css' );
 
 	wp_enqueue_style( $child, get_stylesheet_directory_uri() . '/style.css', array( $parent ) );
+
+	wp_enqueue_style( 'bp-legacy-css', 'https://buddypress.org/wp-content/plugins/buddypress/bp-templates/bp-legacy/css/buddypress.min.css', array( $child ) );
+	wp_enqueue_style( 'bb-base', 'https://buddypress.org/wp-content/themes/bb-base/style.css', array( $child ) );
+	wp_enqueue_style( 'bb-root', 'https://buddypress.org/wp-content/themes/bb-base/style-buddypress.css', array( $child ) );
+	wp_enqueue_style( 'bb-child', 'https://buddypress.org/wp-content/themes/buddypress-org/style.css', array( $child ) );
 }
 add_action( 'wp_enqueue_scripts', 'bporg_developer_enqueue_css' );
