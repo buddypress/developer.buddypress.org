bf36da6f22b8593cff4e62d8ef1090fbff9e5252
Michael Beckwith <tw2113@gmail.com>
2016-06-01 23:14:14 -0500

    remove duplicated functions that will exist in parent theme

--- wp-content/themes/bporg-developer/inc/template-tags.php
+++ wp-content/themes/bporg-developer/inc/template-tags.php
@@ -181,41 +181,6 @@ namespace {
 		}
 	endif;
 
-	/**
-	 * Returns true if a blog has more than 1 category.
-	 */
-	function wporg_developer_categorized_blog() {
-		if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
-			// Create an array of all the categories that are attached to posts.
-			$all_the_cool_cats = get_categories( array(
-				'hide_empty' => 1,
-			) );
-
-			// Count the number of categories that are attached to the posts.
-			$all_the_cool_cats = count( $all_the_cool_cats );
-
-			set_transient( 'all_the_cool_cats', $all_the_cool_cats );
-		}
-
-		if ( '1' != $all_the_cool_cats ) {
-			// This blog has more than 1 category so wporg_developer_categorized_blog should return true.
-			return true;
-		} else {
-			// This blog has only 1 category so wporg_developer_categorized_blog should return false.
-			return false;
-		}
-	}
-
-	/**
-	 * Flush out the transients used in wporg_developer_categorized_blog.
-	 */
-	function wporg_developer_category_transient_flusher() {
-		// Like, beat it. Dig?
-		delete_transient( 'all_the_cool_cats' );
-	}
-
-	add_action( 'edit_category', 'wporg_developer_category_transient_flusher' );
-	add_action( 'save_post', 'wporg_developer_category_transient_flusher' );
 }
 
 namespace DevHub {
