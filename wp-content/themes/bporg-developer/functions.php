337e043c361f0fa0ba66d07b2645baf65179e21a
Michael Beckwith <tw2113@gmail.com>
2016-06-23 02:20:16 -0500

    reduce redundancy in handle name

--- wp-content/themes/bporg-developer/functions.php
+++ wp-content/themes/bporg-developer/functions.php
@@ -9,7 +9,7 @@ function bporg_developer_enqueue_css() {
 
 	wp_enqueue_style( $parent, get_template_directory_uri() . '/style.css' );
 
-	wp_enqueue_style( 'bp-legacy-css', 'https://buddypress.org/wp-content/plugins/buddypress/bp-templates/bp-legacy/css/buddypress.min.css', array( $parent ) );
+	wp_enqueue_style( 'bp-legacy', 'https://buddypress.org/wp-content/plugins/buddypress/bp-templates/bp-legacy/css/buddypress.min.css', array( $parent ) );
 	wp_enqueue_style( 'bb-base', 'https://buddypress.org/wp-content/themes/bb-base/style.css', array( $parent ) );
 	wp_enqueue_style( 'bb-root', 'https://buddypress.org/wp-content/themes/bb-base/style-buddypress.css', array( $parent ) );
 	wp_enqueue_style( 'bb-child', 'https://buddypress.org/wp-content/themes/buddypress-org/style.css', array( $parent ) );
